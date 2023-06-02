000e359cb1f207d4d2cfa0c6ad5e29309381d780
son <jhson0507@gmail.com>
2023-06-02 22:29:56 +0900

    add toonflix prac

---
+++ toonflix/lib/widgets/episode_widget.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+class Episode extends StatelessWidget {
+  const Episode({
+    super.key,
+    required this.episode,
+    required this.webtoonId,
+  });
+  final String webtoonId;
+  final WebtoonEpisodeModel episode;
+
+  onButtonTap() async {
+    // final url = Uri.parse("https://google.com");
+    // await launchUrl(url);
+    await launchUrlString(
+        "https://comic.naver.com/webtoon/detail?titleId=$webtoonId&no=${episode.id}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onButtonTap,
+      child: Container(
+        margin: const EdgeInsets.only(
+          bottom: 10,
+        ),
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 1,
+            color: Colors.green.shade400,
+          ),
+          borderRadius: BorderRadius.circular(30),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 2,
+              offset: const Offset(5, 5),
+              color: Colors.black.withOpacity(0.2),
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                episode.title,
+                style: TextStyle(
+                  color: Colors.green.shade400,
+                  fontSize: 16,
+                ),
+              ),
+              Icon(
+                Icons.chevron_right_rounded,
+                color: Colors.green.shade400,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
