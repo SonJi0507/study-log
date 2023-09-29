bb6c697ab75439108d2947fd61cdaadffe59416f
Son Jihyeok <sonji@Sons-MacBook-Pro.local>
2023-09-29 22:45:00 +0900

    singleton pattern

---
+++ python_design_pattern/singleton/meta_singleton.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+
+class MetaSingleton(type):
+    _instance = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instance:
+            cls._instance[cls] = super(MetaSingleton, cls).__call__(*args, **kwargs)
+        return cls._instance[cls]
+
+class Database(metaclass=MetaSingleton):
+    connection = None
+    def connect(self):
+        if self.connection is None:
+            self.connection = sqlite3.connect("db.sqlite3")
+            self.cursorobj = self.connection.cursor()
+        return self.cursorobj
+    
+db1 = Database().connect()
+db2 = Database().connect()
+
+print("Database Objects DB1", db1)
+print("Database Objects DB2", db2)
+
+"""
+Database Objects DB1 <sqlite3.Cursor object at 0x1028b43c0>
+Database Objects DB2 <sqlite3.Cursor object at 0x1028b43c0>
+"""
---
+++ python_design_pattern/singleton/singleton.py
@@ -0,0 +1,16 @@
+class Singleton(object):
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Singleton, cls).__new__(cls)
+        return cls.instance
+    
+s = Singleton()
+print("Object created", s)
+
+s1 = Singleton()
+print("Object created", s1)
+
+"""
+Object created <__main__.Singleton object at 0x105218fa0>
+Object created <__main__.Singleton object at 0x105218fa0>
+"""
