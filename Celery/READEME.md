8d2dc12c3aef37bd73181183a8a7d6ec9673b3fa
Son Jihyeok <sonji@Sons-MacBook-Pro.local>
2023-12-30 19:01:11 +0900

    기존 Celery 공부 다시 정리하기

---
+++ Celery/READEME.md
@@ -0,0 +1,4 @@
+Celery Prac
+===
+참고: https://docs.celeryq.dev/en/stable/getting-started/index.html
+
--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from api.views import Test, get_task_status
-
-
-urlpatterns = [
-    path('test/', Test.as_view()),
-    path('tasks/<task_id>/', get_task_status, name='get_task_status'),
-]
