e9cb5fe37d6c317d40756b2b1e84eefca22ec60b
sonji <jhson0507@gmail.com>
2025-02-06 23:41:06 +0900

    Add not-found, layout

--- NextJS/app/not-found.tsx
+++ NextJS/app/not-found.tsx
@@ -2,7 +2,6 @@ import Navigation from "../components/navigation";
 
 export default function NotFound() {
     return <div>
-        <Navigation />
         <h1>Not found!</h1>
     </div>
 }
--- NextJS/components/navigation.tsx
+++ NextJS/components/navigation.tsx
@@ -6,8 +6,6 @@ import { useState } from "react";
 
 export default function Navigation() {
     const path = usePathname();
-    const [count, setCount] = useState(0);
-    console.log(path);
     return (
         <nav>
             <ul>
@@ -16,9 +14,6 @@ export default function Navigation() {
                 </li>
                 <li>
                     <Link href="/about-us">About</Link>{path === "/about-us" ? "🔥" : ""}
-                </li>
-                <li>
-                    <button onClick={() => setCount(count + 1)}>{count}</button>
                 </li>
             </ul>
         </nav>
