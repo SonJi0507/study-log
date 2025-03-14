4e262f414a6666b46f7d7d4afc947b3e6d78e434
sonji <jhson0507@gmail.com>
2025-03-14 14:49:02 +0900

    Add styles

--- NextJS/app/(movies)/movies/[id]/page.tsx
+++ NextJS/app/(movies)/movies/[id]/page.tsx
@@ -10,7 +10,6 @@ export default async function MovieDetail({ params }: { params: Params }) {
     const { id } = await params;
     // const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
     return <div>
-        <h3>Movie detail Page</h3>
         <Suspense fallback={<h1>Loading movie info</h1>}>
             <MovieInfo id={id} />
         </Suspense>
